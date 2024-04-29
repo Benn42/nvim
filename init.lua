2a30e618b1d97bbddb272fb6e5182e8ccd58246b
Ben Slough <benslough@live.co.uk>
2024-04-29 21:21:17 +0100

    Updated lsps

--- init.lua
+++ init.lua
@@ -264,18 +264,10 @@ require('lazy').setup({
       capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
 
       local servers = {
-        -- clangd = {},
-        -- gopls = {},
-        -- pyright = {},
-        -- rust_analyzer = {},
-        -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-        --
-        -- Some languages (like typescript) have entire language plugins that can be useful:
-        --    https://github.com/pmizio/typescript-tools.nvim
-        --
-        -- But for many setups, the LSP (`tsserver`) will work just fine
-        -- tsserver = {},
-        --
+        gopls = {},
+        rust_analyzer = {},
+        tsserver = {},
+        tailwindcss = {},
 
         lua_ls = {
           -- cmd = {...},
