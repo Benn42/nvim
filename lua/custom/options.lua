d30de6e8f1fcbd2b6f4fd5d6fc1fc0cc06de4a78
Ben Slough <benslough@live.co.uk>
2024-06-09 11:29:27 +0100

    Added tabstop configuration

--- lua/custom/options.lua
+++ lua/custom/options.lua
@@ -6,6 +6,8 @@ vim.opt.showmode = false
 vim.opt.clipboard = 'unnamedplus'
 
 vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+vim.opt.tabstop = 4
 vim.opt.expandtab = true
 
 vim.opt.swapfile = false
