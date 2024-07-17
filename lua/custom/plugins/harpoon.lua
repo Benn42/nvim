61d6b46c7865ab668773b8e14840f60425b26204
Ben Slough <benslough@live.co.uk>
2024-07-17 10:46:39 +0100

    updated harpoon to have delete

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -12,8 +12,21 @@ return {
     local conf = require('telescope.config').values
     local function toggle_telescope(harpoon_files)
       local file_paths = {}
+
       for _, item in ipairs(harpoon_files.items) do
         table.insert(file_paths, item.value)
+      end
+
+      local make_finder = function()
+        local paths = {}
+
+        for _, item in ipairs(harpoon_files.items) do
+          table.insert(paths, item.value)
+        end
+
+        return require('telescope.finders').new_table {
+          results = paths,
+        }
       end
 
       require('telescope.pickers')
@@ -24,6 +37,20 @@ return {
           },
           previewer = conf.file_previewer {},
           sorter = conf.generic_sorter {},
+          attach_mappings = function(prompt_buffer_number, map)
+            map('i', '<c-d>', function()
+              local state = require 'telescope.actions.state'
+              local selected_entry = state.get_selected_entry()
+              local current_picker = state.get_current_picker(prompt_buffer_number)
+
+              print(harpoon:list())
+              harpoon:list():remove(selected_entry)
+              print(harpoon:list())
+              current_picker:refresh(make_finder())
+            end)
+
+            return true
+          end,
         })
         :find()
     end
